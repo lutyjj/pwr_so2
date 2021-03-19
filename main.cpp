e5ea9a46b54e254d67e5ee6563a70c1759549d75
Daniil Rylski <addanik@gmail.com>
2021-03-19 20:24:13 +0100

    Preparation

--- main.cpp
+++ main.cpp
@@ -2,25 +2,49 @@
 #include <ncurses.h>
 #include <chrono>
 #include <thread>
+#include <vector>
+
+using namespace std;
 
 struct Car {
-    int pos_x = 0;
-    int pos_y = 0;
+    int current_x = 0;
+    int current_y = 0;
     int speed = 0;
 };
 
+void thread_func(Car *car) {
+    for (int i = 0; i < 100; i++) {
+        car->current_x = i;
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(33));
+    }
+}
+
 int main() {
-    initscr();			/* Start curses mode 		  */
-	
-    for (int i = 0; i < LINES; i++) {
+
+    vector<thread> threads;
+    Car *cars[2];
+
+    for (int i = 0; i < 2; i++) {
+        cars[i] = new Car();
+        threads.emplace_back(thread_func, ref(cars[i]));
+    }
+
+    initscr();
+    while (true) {
         clear();
-        std::this_thread::sleep_for(std::chrono::milliseconds(50));
-        mvaddch(0, i, '.');
+        std::this_thread::sleep_for(std::chrono::milliseconds(33));
+
+        for (int i = 0; i < 2; i++) {
+            mvaddch(0, cars[i]->current_x, '.');
+        }
         refresh();
     }
 
-    getch();
-	endwin();			/* End curses mode		  */
+	endwin();
 
+    for (std::thread & t : threads) {
+        t.join();
+    }
     return 0;
 }
