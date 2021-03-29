1c3d25cc3ab5eb87a20f69fde2df9c87d572906b
Daniil Rylski <addanik@gmail.com>
2021-03-29 22:42:27 +0200

    Possibly fix reverse car movement

--- main.cpp
+++ main.cpp
@@ -7,14 +7,16 @@
 #include <atomic>
 
 #define LOOPS 3
+#define PADDING_X 2
+#define PADDING_Y 1
 
 using namespace std;
 atomic_bool stop_flag;
 
 struct Car
 {
-    int current_x = 0;
-    int current_y = 0;
+    int current_x = 0 + PADDING_X;
+    int current_y = 0 + PADDING_Y;
     float speed = 0;
     int number;
     int loop = 0;
@@ -35,11 +37,11 @@ struct Car
 
     void thread_func()
     {
-        float x = 0;
-        float y = 0;
+        float x = 0 + PADDING_X;
+        float y = 0 + PADDING_Y;
 
-        int cols = COLS - 1;
-        int lines = LINES - 1;
+        int cols = COLS - PADDING_X - 1;
+        int lines = LINES - PADDING_Y - 1;
 
         while (loop < LOOPS)
         {
@@ -64,24 +66,24 @@ struct Car
                 std::this_thread::sleep_for(std::chrono::milliseconds(100));
             }
 
-            while (current_x > 0)
+            while (current_x > PADDING_X)
             {
                 this->current_x = static_cast<int>(x);
                 x -= speed * 1.4;
 
-                if (x < 0)
-                    x = 0;
+                if (x < PADDING_X)
+                    x = PADDING_X;
 
                 std::this_thread::sleep_for(std::chrono::milliseconds(100));
             }
 
-            while (current_y > 0)
+            while (current_y > PADDING_Y)
             {
                 this->current_y = static_cast<int>(y);
                 y -= speed * 0.8;
 
-                if (y < 0)
-                    y = 0;
+                if (y < PADDING_Y)
+                    y = PADDING_Y;
 
                 std::this_thread::sleep_for(std::chrono::milliseconds(100));
             }
@@ -107,7 +109,8 @@ int kbhit(void)
     }
 }
 
-void spawn_car(vector<Car *> &cars) {
+void spawn_car(vector<Car *> &cars)
+{
     int count = 0;
     while (!stop_flag)
     {
@@ -117,11 +120,23 @@ void spawn_car(vector<Car *> &cars) {
     }
 }
 
+void rectangle(int y1, int x1, int y2, int x2)
+{
+    mvhline(y1, x1, 0, x2 - x1);
+    mvhline(y2, x1, 0, x2 - x1);
+    mvvline(y1, x1, 0, y2 - y1);
+    mvvline(y1, x2, 0, y2 - y1);
+    mvaddch(y1, x1, ACS_ULCORNER);
+    mvaddch(y2, x1, ACS_LLCORNER);
+    mvaddch(y1, x2, ACS_URCORNER);
+    mvaddch(y2, x2, ACS_LRCORNER);
+}
+
 int main()
 {
     stop_flag = false;
 
-    WINDOW* win = initscr();
+    WINDOW *win = initscr();
     nodelay(stdscr, TRUE);
     curs_set(0);
 
@@ -133,7 +148,9 @@ int main()
         clear();
         std::this_thread::sleep_for(std::chrono::milliseconds(33));
 
-            box(win, 0, 0);
+        //box(win, 0, 0);
+        rectangle(0, 0, LINES - 1, COLS - 1);
+        rectangle(2, 4, LINES - 3, COLS - 5);
 
         for (auto car : cars)
         {
@@ -142,7 +159,8 @@ int main()
         }
         refresh();
 
-        if (kbhit()) {
+        if (kbhit())
+        {
             stop_flag = true;
             break;
         }
